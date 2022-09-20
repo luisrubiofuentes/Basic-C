fa33b245fa3c69e4686ff4c009e9eb7fd7c3cebc
luisrubiofuentes <113974603+luisrubiofuentes@users.noreply.github.com>
2022-09-20 10:08:32 +0200

    Rename Untitled1.c to Hola_mundo.c

---
+++ Hola_mundo.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int 4; /*guarda números enteros*/
+float x;
+double y; /*float y double para decimales, pero uno es más preciso por así decirlo. Si pusiese signed estaría en un intervalo*/
+char z; /*se usan  las comillas simples ('a')*/
+
+int main (){
+    printf("hola clase\n"); /*carlos*/
+
+};
